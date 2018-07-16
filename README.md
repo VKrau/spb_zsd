f25c212a3d88dfffcca153788272eddb9d7c5906
VKrau <atx.korchagin@gmail.com>
2018-07-16 16:22:46 +0300

    added parameter avoid

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # spb_zsd
-python travel_calс.py --orig latitude,longitude --dest latitude,longitude, где --orig - координаты точки отправления (точка А), --dest - координаты точки назначения (точка Б).
+python travel_calс.py --orig latitude,longitude --dest latitude,longitude --avoid tolls,highways 
+	где --orig (обязательный параметр) - координаты точки отправления (точка А);
+		--dest (обязательный параметр) - координаты точки назначения (точка Б);
+		--avoid (необязательный параметр) - исключить: tolls - платные участки, highways - шоссе, ferries - паромные переправы, indoor - ?
 Пример вызова из командной строки:
-python travel_calc.py --orig 59.97241,30.2133597, --dest 59.9177427,30.2103535
+python travel_calc.py --orig 59.97241,30.2133597 --dest 59.9177427,30.2103535 --avoid tolls,highways
 
 Пример вызова функции:
-start_calc((59.97241,30.2133597),(59.9177427,30.2103535))
+start_calc("59.97241,30.2133597", "59.9177427,30.2103535", "tolls,highways")
--- travel_calc.py
+++ travel_calc.py
@@ -8,34 +8,40 @@ from __future__ import absolute_import
 from __future__ import print_function
 import simplejson
 import urllib
-import sys
 import re
+import argparse
 
-def coord2text(coord):
-    return ",".join(map(lambda x: str(x), coord))
-
-def start_calc(orig, dest):
+def start_calc(orig, dest, avoid=''):
     #orig_coord = 59.97241,30.2133597
     #dest_coord = 59.9177427,30.2103535
     orig_coord = orig
     dest_coord = dest
-    url = str("http://maps.googleapis.com/maps/api/distancematrix/json?origins=%s&destinations=%s&mode=driving&language=en-EN&sensor=false" % 
-             (coord2text(orig_coord), coord2text(dest_coord)))
-    
+    if avoid:
+        avoid = re.sub(r',', '|', avoid)
+    url = ''.join(str("http://maps.googleapis.com/maps/api/distancematrix/json?origins=%s\
+              &destinations=%s\
+              &mode=driving\
+              &avoid=%s\
+              &language=en-EN" %
+             (orig_coord, dest_coord, avoid)).split())
     result = simplejson.load(urllib.urlopen(url))
     distance = result['rows'][0]['elements'][0]['distance']
     driving_time = result['rows'][0]['elements'][0]['duration']
-    
+
     print("From: %s %s" % (result['origin_addresses'][0], orig_coord))
     print("To: %s %s" % (result['destination_addresses'][0], dest_coord))
     print("Distance: %s. (%s m.)" % (distance['text'], distance['value']))
     print("Driving Time: %s. (%s sec.)" % (driving_time['text'], driving_time['value']))
-    
+
 if __name__ == "__main__":
-    argv_string = ' '.join(sys.argv[1:])
-    orig = re.findall(r'--orig (\w+.\w+,\w+.\w+)', argv_string)
-    dest = re.findall(r'--dest (\w+.\w+,\w+.\w+)', argv_string)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--orig', required=True)
+    parser.add_argument('--dest', required=True)
+    parser.add_argument('--avoid')
+
     try:
-        start_calc(orig, dest)
+        start_calc(parser.parse_args().orig,
+                   parser.parse_args().dest,
+                   parser.parse_args().avoid)
     except:
-        print("Please enter the correct coordinates!")
+        print("Please enter the correct coordinates or parameters!")
