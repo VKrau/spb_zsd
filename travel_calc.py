7d92818e63a4956b01e3bd48695014e4706c9f25
VKrau <atx.korchagin@gmail.com>
2018-07-19 13:30:12 +0300

    small code cleaning

--- travel_calc.py
+++ travel_calc.py
@@ -13,10 +13,6 @@ import sys
 import argparse
 
 def start_calc(orig, dest, avoid, traffic_model, key):
-    #orig_coord = 59.97241,30.2133597
-    #dest_coord = 59.9177427,30.2103535
-    orig_coord = orig
-    dest_coord = dest
     if avoid:
         avoid = re.sub(r",", "|", avoid)
     url = ''.join(str("http://maps.googleapis.com/maps/api/distancematrix/json?origins=%s\
@@ -27,8 +23,7 @@ def start_calc(orig, dest, avoid, traffic_model, key):
               &traffic_model=%s\
               &departure_time=now\
               &key=%s" %
-             (orig_coord, dest_coord, avoid, traffic_model, key)).split())
-    print(url)
+             (orig, dest, avoid, traffic_model, key)).split())
     try:
         result = simplejson.load(urllib.urlopen(url))
     except IOError:
@@ -40,8 +35,8 @@ def start_calc(orig, dest, avoid, traffic_model, key):
     except:
         print("Error! Please enter the correct coordinates or parameters!")
         sys.exit()
-    print("From: %s %s" % (result["origin_addresses"][0], orig_coord))
-    print("To: %s %s" % (result["destination_addresses"][0], dest_coord))
+    print("From: %s %s" % (result["origin_addresses"][0], orig))
+    print("To: %s %s" % (result["destination_addresses"][0], dest))
     print("Distance: %s. (%s m.)" % (distance["text"], distance["value"]))
     print("Driving Time: %s. (%s sec.)" % (driving_time["text"], driving_time["value"]))
 
@@ -65,7 +60,6 @@ if __name__ == "__main__":
     parser.add_argument("--key", default="", help="Your application's API key. This key\
                        identifies your application for purposes of quota\
                        management")
-    print(parser.parse_args().traffic_model)
     start_calc(parser.parse_args().orig,
                parser.parse_args().dest,
                parser.parse_args().avoid,
